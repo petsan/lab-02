43e1f2f945ab144378e3ee03182f0610b1c96c5d
Peter Sankiewicz <petsan@hotmail.com>
2020-06-03 15:11:48 -0700

    lab 03 mustache

--- README.md
+++ README.md
@@ -20,3 +20,39 @@ Feature 1: 3-4 hours
 Feature 2: 2-3 hours
 
 Feature 3: 15 minutes
+
+## Time Estimates Lab 3
+
+
+
+Number and name of feature: Pagination
+
+Estimate of time needed to complete: 4
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+
+Number and name of feature: Templating
+
+Estimate of time needed to complete: 2
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+
+Number and name of feature: Styling with Flexbox
+
+Estimate of time needed to complete: 1
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- js/app.js
+++ js/app.js
@@ -1,49 +1,85 @@
 'use strict'
 
-let allHorns = [];
-let keywords = [];
+// let allHorns = [];
+// let keywords = [];
 
-function Horns(obj){
-  this.title = obj.title;
-  this.description = obj.description;
-  this.image_url = obj.image_url;
-  this.keyword = obj.keyword;
-  this.horns = obj.horns
+// function Horns(obj){
+//   this.title = obj.title;
+//   this.description = obj.description;
+//   this.image_url = obj.image_url;
+//   this.keyword = obj.keyword;
+//   this.horns = obj.horns
 
-  allHorns.push(this);
-  if(!keywords.includes(this.keyword)){
-    keywords.push(this.keyword);
+//   allHorns.push(this);
+//   if(!keywords.includes(this.keyword)){
+//     keywords.push(this.keyword);
+//   }
+// }
+let pages = [];
+let pageDataset = [
+  {
+    'image_url': 'http://cdn.shopify.com/s/files/1/0288/5332/files/jackalope_in_the_wild_large.jpg?11502701639502724114',
+    'title': 'Jackalope',
+    'description': 'You know, just a jackalope in there for good measure. I know they don\'t have horns, but they have antlers. And they may or may not be real.',
+    'keyword': 'jackalope',
+    'horns': 2
   }
+];
+
+function Page(obj){
+  for(let key in obj){
+    this[key] = obj[key]
+  }
+  pages.push(this);
 }
 
-Horns.prototype.render = function(){
-  const myTemplate = $('#photo-template').html();
-  const $newSection = $(`<section class="${this.keyword}">${myTemplate}</section>`);
-  $newSection.find('h2').text(this.title);
-  $newSection.find('p').text(this.description);
-  $newSection.find('img').attr('src', this.image_url);
-  $('main').append($newSection);
+pageDataset.forEach(page => {
+  new Page(page);
+})
+
+
+Page.prototype.toHtml = function(){
+  let template = $('#page-template').html();
+  let html = Mustache.render(template, this);
+  return html;
 }
 
-$.ajax('data/page-1.json', {method: 'GET', dataType: 'JSON'})
-  .then(horns => {
-    horns.forEach(value => {
-      new Horns(value).render();
-    })
-  }).then(()=> {
-    keywords.forEach((keyword) =>{
-      let stuff = `<option value = "${keyword}">${keyword}</option>`
-      $('select').append(stuff);
-    })
-  });
+pages.forEach(page => {
+  // this will create the html
+  let pageHtml = page.toHtml();
+
+  // append to page
+  $('#page').append(pageHtml);
+})
+
+// Horns.prototype.render = function(){
+//   const myTemplate = $('#photo-template').html();
+//   const $newSection = $(`<section class="${this.keyword}">${myTemplate}</section>`);
+//   $newSection.find('h2').text(this.title);
+//   $newSection.find('p').text(this.description);
+//   $newSection.find('img').attr('src', this.image_url);
+//   $('main').append($newSection);
+// }
+
+// $.ajax('data/page-1.json', {method: 'GET', dataType: 'JSON'})
+//   .then(horns => {
+//     horns.forEach(value => {
+//       new Horns(value).render();
+//     })
+//   }).then(()=> {
+//     keywords.forEach((keyword) =>{
+//       let stuff = `<option value = "${keyword}">${keyword}</option>`
+//       $('select').append(stuff);
+//     })
+//   });
 
 
-$('select').on('change', function() {
-  let $variable = $(this).val();
-  if ($variable === 'default') {
-    $('section').show();
-  } else {
-  $('section').hide();
-  $(`section[class="${$variable}"]`).show();
-}
-});
+// $('select').on('change', function() {
+//   let $variable = $(this).val();
+//   if ($variable === 'default') {
+//     $('section').show();
+//   } else {
+//   $('section').hide();
+//   $(`section[class="${$variable}"]`).show();
+// }
+// });
