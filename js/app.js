3236ee8682721171eac2b760be9aa11399f9a29d
Peter Sankiewicz <petsan@hotmail.com>
2020-06-04 16:05:30 -0700

    wip for sort

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 let allHorns = [];
 let keywords = [];
 let page = 'data/page-1.json';
+let sortOrder = 'sortAlpha'
+console.log(allHorns);
 
 function Horns(obj){
   this.title = obj.title;
@@ -17,6 +19,19 @@ function Horns(obj){
   }
 }
 
+const applySortOrder = (sortOrder) => {
+  if (sortOrder === 'sortAlpha') {
+    allHorns.sort((a,b) => { return a.title > b.title ? 1 : -1; });
+  } else if (sortOrder === 'sortNum') {
+    allHorns.sort((a,b) => { return a.horns - b.horns })
+  } else {
+    return allHorns;
+  }
+}
+
+applySortOrder('sortNum'); //this is how to call it
+
+
 Horns.prototype.render = function(){
   const template = $('#photos-template').html();
   const myTemplate = Mustache.render(template, this);
@@ -26,11 +41,15 @@ Horns.prototype.render = function(){
 const pageLoad = (page) => {
   $('main').empty();
   $('select').empty();
+  console.log(allHorns)
+  applySortOrder('sortNum'); //this is how to call it
+  console.log(allHorns)
   let defaultOption = $('<option value="default">Filter by Keyword</option>')
   $('select').append(defaultOption);
   $.ajax(`${page}`, {method: 'GET', dataType: 'JSON'})
     .then(horns => {
       horns.forEach(value => {
+        // this needs to be broken off and shove an extra step:sorting
         new Horns(value).render();
       }) //look for lines 46-52 in demo
     }).then(()=> {
